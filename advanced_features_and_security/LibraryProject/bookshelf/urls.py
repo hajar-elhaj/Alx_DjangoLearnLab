1a43fcccb1f4990103f678eacab45ba7545835d0
Hajar <hajar.elhaj20@gmail.com>
2026-01-25 18:08:42 +0100

    Managing Permissions and Groups in Django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+"""
+Permissions & Groups Setup:
 
-# Create your views here.
+Custom permissions are defined on the Book model:
+- can_view
+- can_create
+- can_edit
+- can_delete
+
+Groups:
+- Viewers: can_view
+- Editors: can_view, can_create, can_edit
+- Admins: all permissions
+
+Views are protected using @permission_required decorator.
+"""
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect
+from .models import Book
+
+
+#View a book
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/view_books.html', {'books': books})
+
+
+#Create a book
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        year = request.POST.get('publication_year')
+
+        Book.objects.create(
+            title=title,
+            author=author,
+            publication_year=year
+        )
+
+    return render(request, 'bookshelf/create_book.html')
+
+
+
+# Edit an existing book
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = Book.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+
+# Delete a book
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = Book.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        book.delete()
+
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
