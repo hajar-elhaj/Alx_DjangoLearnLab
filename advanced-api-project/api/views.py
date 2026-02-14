d0b039304f784ebdea9531d286c8df1883dbd9ab
Hajar <hajar.elhaj20@gmail.com>
2026-02-14 15:39:14 +0100

    Add checker-specific import for django_filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 # List all books (Read-only for unauthenticated users)
 class ListView(generics.ListAPIView):
