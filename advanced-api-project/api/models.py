d59362ec3493bbe614c037ac452de7809ae6f473
Hajar <hajar.elhaj20@gmail.com>
2026-02-14 15:31:46 +0100

    Building Custom Views and Generic Views in Django REST Framewok

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,31 +1,17 @@
 from django.db import models
 
-
-# Author Model
-# This model represents an author who can have multiple books.
 class Author(models.Model):
-    # Stores the name of the author
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        # This makes the author name readable in Django admin
         return self.name
 
-
-# Book Model
-# This model represents a book written by an author.
 class Book(models.Model):
-    # Stores the book title
     title = models.CharField(max_length=200)
-
-    # Stores the year the book was published
     publication_year = models.IntegerField()
-
-    # ForeignKey creates a one-to-many relationship:
-    # One Author can have many Books.
     author = models.ForeignKey(
         Author,
-        related_name='books',  # Allows access via author.books
+        related_name='books',
         on_delete=models.CASCADE
     )
 
