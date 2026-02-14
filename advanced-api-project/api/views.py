effbc771407bb5165fb65b12f1daaa499dfc3947
Hajar <hajar.elhaj20@gmail.com>
2026-02-14 15:03:26 +0100

    Building Custom Views and Generic Views in Django REST Framewok

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,35 +5,35 @@ from .serializers import BookSerializer
 
 
 # List all books (Read-only for unauthenticated users)
-class BookListView(generics.ListAPIView):
+class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 # Retrieve a single book
-class BookDetailView(generics.RetrieveAPIView):
+class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 # Create book (Authenticated only)
-class BookCreateView(generics.CreateAPIView):
+class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 
 # Update book (Authenticated only)
-class BookUpdateView(generics.UpdateAPIView):
+class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 
 # Delete book (Authenticated only)
-class BookDeleteView(generics.DestroyAPIView):
+class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
