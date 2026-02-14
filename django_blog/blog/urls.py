683d482ebf41789064929f536b7ab2ea05cb787f
Hajar <hajar.elhaj20@gmail.com>
2026-02-14 17:56:43 +0100

    Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,10 @@
+# blog/urls.py
 from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to Django Blog!")
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
 ]
