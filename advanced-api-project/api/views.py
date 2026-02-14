baa31843d102a1a2e07af0abe40b317bf8a683ad
Hajar <hajar.elhaj20@gmail.com>
2026-02-14 15:35:43 +0100

    Implement filtering, search, and ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,14 +1,30 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from .models import Book
 from .serializers import BookSerializer
-
 
 # List all books (Read-only for unauthenticated users)
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Enable filtering, search, and ordering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    # Fields allowed for filtering
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Fields allowed for search
+    search_fields = ['title', 'author__name']
+
+    # Fields allowed for ordering
+    ordering_fields = ['title', 'publication_year']
+
+    # Default ordering
+    ordering = ['title']
 
 
 # Retrieve a single book
