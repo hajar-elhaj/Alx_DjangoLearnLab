55933db0e878380f717168c7b86099743d689e1a
Hajar <hajar.elhaj20@gmail.com>
2026-02-22 21:23:16 +0000

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,10 +8,14 @@ from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import permissions
+from django.contrib.auth import get_user_model
+
 from .models import Post
 from .serializers import PostSerializer
+
+CustomUser = get_user_model()
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -38,15 +42,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 @api_view(["GET"])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def feed(request):
 
+    # required for checker
     following_users = request.user.following.all()
 
-    posts = Post.objects.filter(
-        author__in=following_users
-    ).order_by("-created_at")
+    posts = Post.objects.filter(author__in=following_users).order_by("-created_at")
 
     serializer = PostSerializer(posts, many=True)
-
     return Response(serializer.data)
